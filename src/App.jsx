dbeba2e482e7ddaaba1dafeb92e68fed1b8c33dc
dr-winner <132192045+dr-winner@users.noreply.github.com>
2025-02-28 13:13:56 +0000

    demo 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Count from "./components/Count"
+
+
+function App() {
+
+  return (
+    <>
+     {/* < Hello name="Winner" role="Student" age="15"/> */}
+     <Count/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hello.jsx
@@ -0,0 +1,10 @@
+function Hello({name, age, role}) {
+
+    return <>
+    <h1>Hello {name}</h1>
+    <h1>You are {age} years old</h1>
+    <h1>You are a {role}</h1>
+    </>
+}
+
+export default Hello;
